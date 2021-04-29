89c7bb216bb99640f8e34f4f7e2da6df7dad4791
Topher Johnson <wolfthejohnson@gmail.com>
2021-04-29 18:54:43 -0500

    added pond route

--- routes/home.js
+++ routes/home.js
@@ -1,11 +1,14 @@
 // bring in variables inlcuding express, router, and the homeController
 const express = require('express')
 const router = express.Router()
+const {ensureAuth, ensureGuest} = require('../middleware/auth')
 const homeController = require('../controllers/home')
 
 // get request to the root 
 // runs getIndex method with is within the homeController
-router.get('/', homeController.getIndex) 
+router.get('/', homeController.getIndex)
+router.get('/lake', )
+router.get('/pond/:id', /* ensureAuth, */ homeController.getPond)
 
 // exporting the router variable so anyone using this module can use the router that we've created
 module.exports = router

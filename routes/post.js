ede4e90854edf05a75c20746a681ef448348562c
medium0fry <medium0fry@gmail.com>
2021-04-28 20:41:12 -0400

    Repurposed todo routes into post controller

---
+++ routes/post.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const router = express.Router()
+
+const postController = require('../controllers/post')
+// requires that the user is logged in/authorized 
+const { ensureAuth, ensureGuest } = require('../middleware/auth')
+
+// @desc    Create a post
+// @route   GET /post
+router.get('/', /*ensureAuth,*/ postController.addPost);
+
+// @desc    View a specific post
+// @route   GET /post/:id
+router.get('/:id', postController.viewPost);
+
+// @desc    Edit a specific post
+// @route   PUT /post/:id
+router.put('/:id', postController.editPost);
+
+// @desc    Delete a post
+// @route   DELETE /post/:id
+router.delete('/:id', postController.deletePost);
+
+// @desc    Like a post
+// @route   PUT /post/:id/like
+router.put('/:id/like', postController.likePost);
+
+module.exports = router;

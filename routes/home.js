d61f5067fd753a93251fd1e1a075143b4112990b
Adeel Bukhari <adeelbukhari@gmail.com>
2021-04-29 21:38:47 -0400

    Added routes

--- routes/home.js
+++ routes/home.js
@@ -3,12 +3,16 @@ const express = require('express')
 const router = express.Router()
 const {ensureAuth, ensureGuest} = require('../middleware/auth')
 const homeController = require('../controllers/home')
+const authController = require('../controllers/auth')
 
 // get request to the root 
 // runs getIndex method with is within the homeController
 router.get('/', homeController.getIndex)
-router.get('/lake', )
+// router.get('/lake', )
 router.get('/pond/:id', /* ensureAuth, */ homeController.getPond)
+router.get('/signup', authController.getSignup)
+//router.post('/signup', authController.postSignup)
+router.get('logout', authController.getLogout)
 
 // exporting the router variable so anyone using this module can use the router that we've created
 module.exports = router

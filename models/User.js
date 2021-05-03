326ed6a1bc439c6e424c29c49794ffe1b8d6d981
medium0fry <medium0fry@gmail.com>
2021-05-03 15:52:12 -0400

    Added password hashing to User model

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 // creates mongoose schema for users
 // requires micorsoftID and displayname, both strings
@@ -26,5 +27,29 @@ const UserSchema = new mongoose.Schema({
     required: true
   }
 })
+
+// Password hash middleware.
+UserSchema.pre('save', function save(next) {
+   const user = this;
+   if (!user.isModified('password')) return next();
+   bcrypt.genSalt(10, (err, salt) => {
+      if (err) return next(err);
+      bcrypt.hash(user.password, salt, (err, hash) => {
+         if (err) return next(err);
+         user.password = hash;
+         next();
+      });
+   });
+});
+
+// Helper to validate user's password
+UserSchema.methods.comparePassword = function comparePassword(
+   candidatePassword,
+   cb
+) {
+   bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+      cb(err, isMatch);
+   });
+};
 
 module.exports = mongoose.model('User', UserSchema)

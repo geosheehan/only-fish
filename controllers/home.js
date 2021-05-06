ff9b06334abd59c5a4e3f114845f4285a25b0fee
medium0fry <medium0fry@gmail.com>
2021-05-06 16:30:43 -0400

    Showing private posts to post authors on their pond. Added references to posts on pond pages.

--- controllers/home.js
+++ controllers/home.js
@@ -3,20 +3,20 @@ const Post = require('../models/Post')
 
 // exporting getIndex
 module.exports = {
-    getIndex: (req,res)=>{
-      // telling response to renders index ejs
-        res.render('login.ejs')
-    },
-    getPond: async (req, res) => {
-      try {
-          const user =  await User.findOne({ userName: req.params.username })
-          if (null === user) return res.render('../views/errors/404');
+  getIndex: (req,res)=>{
+    // telling response to renders index ejs
+      res.render('login.ejs')
+  },
+  getPond: async (req, res) => {
+    try {
+      const author =  await User.findOne({ userName: req.params.username })
+      if (null === author) return res.render('../views/errors/404');
 
-          const posts = await Post.find({ user: user._id, status: 'public' })
-          res.render('pond.ejs', { user, posts })
-      } catch (err) {
-          console.error(err)
-      }
+      const posts = await Post.find({ user: author._id })
+      res.render('pond.ejs', { author, posts, user: req.user })
+    } catch (err) {
+        console.error(err)
+    }
   },
   getLake: async (req, res) => {
     try {

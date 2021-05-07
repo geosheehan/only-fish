c3a39579efbfd0aa8593913f0e2826d8c1a61127
shenando <shenandoah.grace@gmail.com>
2021-05-06 20:48:34 -0400

    add upload to routes post.js

--- routes/post.js
+++ routes/post.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-
+const upload = require("../middleware/multer");
 const postController = require('../controllers/post')
 // requires that the user is logged in/authorized 
 const { ensureAuth, ensureGuest } = require('../middleware/auth')
